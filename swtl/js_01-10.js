52540eed770f52bc56edf3e0cee9c73d637a12d0
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-09 09:22:22 -0500

    Update js_01-10.js

--- swtl/js_01-10.js
+++ swtl/js_01-10.js
@@ -3,8 +3,13 @@
 alert("Welcome to Kyko Kombucha!");
 var firstName = prompt("Please enter your first name: ", "First Name Here");
 var lastName = prompt("Please enter your last name: ", "Last Name Here");
-var age = prompt("Please enter your age in years: ", "Enter age here");
 var fullName = firstName + " " + lastName;
+
+var age = prompt("Please enter your age in years: ", "Enter age here");
+if (age.typeOf === 'String') {
+    age = prompt("Please enter your age in years: ", "Enter a number here");
+}
+
 
 if (age < 18){
     var daysLeft = (18 - age) * 365;
