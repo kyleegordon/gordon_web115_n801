fa6895047c4daff824890d4a063244458ae257df
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-09 09:14:58 -0500

    Create js_ch1-10.js

---
+++ swtl/js_ch1-10.js
@@ -0,0 +1,15 @@
+
+
+alert("Welcome to Kyko Kombucha!");
+var firstName = prompt("Please enter your first name: ", "First Name Here");
+var lastName = prompt("Please enter your last name: ", "Last Name Here");
+var age = prompt("Please enter your age in years: ", "Enter age here");
+var fullName = firstName + lastName;
+
+if (age < 18){
+    var daysLeft = (18 - age) * 365;
+    alert(`Hello ${fullName}, only ${daysLeft} days until you can enjoy our delicious kombucha!`);
+    
+} else{
+    alert(`Hello ${fullName}, please enjoy a refreshing kombucha!`);
+}
