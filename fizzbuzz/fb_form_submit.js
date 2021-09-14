a84a006037af90e8e43c5eaa749f74a5e89256b4
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-14 11:23:36 -0500

    Update fb_form_submit.js

--- fizzbuzz/fb_form_submit.js
+++ fizzbuzz/fb_form_submit.js
@@ -1,4 +1,3 @@
-alert("test");
 
 function getData() {
   let firstName = document.getElementById('first_name').value;
@@ -6,4 +5,14 @@ function getData() {
   let middleInitial = document.getElementById('middle_initial').value;
         
    document.getElementById("greeting").innerText = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
+  bubbleSip();
+}
+
+function bubbleSip() {
+  
+  for(let i = 1; i < 125; i+= 2) {
+    document.getElementById("output").innerHTML = "<li>" + i + "Bubble</li>";
+    document.getElementById("output").innerHTML = "<li>" + (i + 1) + "Sip</li>";
+  }
+  
 }
