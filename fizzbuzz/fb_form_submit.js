dc148c4235fb901fa63271ce2fd3bc04029e231a
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-14 11:28:13 -0500

    Update fb_form_submit.js

--- fizzbuzz/fb_form_submit.js
+++ fizzbuzz/fb_form_submit.js
@@ -4,15 +4,18 @@ function getData() {
   let lastName = document.getElementById('last_name').value;
   let middleInitial = document.getElementById('middle_initial').value;
         
-   document.getElementById("greeting").innerText = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
+   document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
   bubbleSip();
 }
 
 function bubbleSip() {
   
   for(let i = 1; i < 125; i+= 2) {
-    document.getElementById("output").innerHTML = "<li>" + i + "Bubble</li>";
-    document.getElementById("output").innerHTML = "<li>" + (i + 1) + "Sip</li>";
+    let bubble = "<li>" + i + "Bubble</li>";
+    let sip = "<li>" + (i + 1) + "Sip</li>";
+    
+    document.getElementById("output").innerHTML = bubble;
+    document.getElementById("output").innerHTML = sip;
   }
   
 }
