dae7db5a6c08330c9bba3d6ee3df5be9b74ec36f
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-17 11:59:06 -0500

    Update js_11-20.js

--- swtl/js_11-20.js
+++ swtl/js_11-20.js
@@ -1,1 +1,34 @@
--
+function orderTotal() {
+  let total = addTotals(getSelected());
+  
+}
+
+function getSelected() {
+        //create array to hold selections
+        let selected = new Array();
+ 
+        //references the form
+        let beverages = document.getElementById("menu");
+ 
+        //references all checkboxes in the form
+        let checks = beverages.getElementsByTagName("INPUT");
+ 
+        // loops to push checked selections to an array
+        for (var i = 0; i < checks.length; i++) {
+            if (checks[i].checked) {
+                selected.push(checks[i].value);
+            }
+        }
+  return selected;
+}
+
+function addTotal(selectedPrices) {
+  let sum = 0;
+  
+  // adds each price to sum
+  for (var i = 0; i < selectedPrices.length; i++) {
+    sum += selectedPrices[i]; 
+  }
+  
+  return total;
+}
