f40af72e0a024e10043f98ff08b0cf8be3455555
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-09-20 14:09:58 -0500

    Create form_submit2.js

---
+++ fizzbuzz/form_submit2.js
@@ -0,0 +1,45 @@
+function getData() {
+  let firstName = document.getElementById('first_name').value;
+  let lastName = document.getElementById('last_name').value;
+  let middleInitial = document.getElementById('middle_initial').value;
+        
+   document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
+  bubbleSip();
+}
+
+function bubbleSip() {
+  for (let i = 1; i <= 140; i++) {
+    var word = '';
+    
+    if (i % 3 === 0) {
+      word = word.concat('Sip');
+    } 
+    
+    if (i % 5 === 0) {
+      word = word.concat('SLURP!');
+    }
+    
+    if (word === '') {
+      word = 'Bubble';
+    }
+      
+      
+    
+    //creates new element & stores in variable
+    var newEl = document.createElement('li');
+    
+    //creates text node & stores in variable
+    var newText = document.createTextNode(i + " " + word);
+    
+    //attaches text node to new element
+    newEl.appendChild(newText);
+    
+    //find position where element should be added
+    var position = document.getElementsByTagName('ul')[0];
+    
+    //insert new element in correct position
+    position.appendChild(newEl);
+    
+  }
+  
+}
