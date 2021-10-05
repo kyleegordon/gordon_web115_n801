328b50162fdc383f6315cc4eabf173441db5a383
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-10-05 12:11:12 -0500

    Update js_11-20.js
    Adjusted {} alignment

--- scripts/js_11-20.js
+++ scripts/js_11-20.js
@@ -1,15 +1,12 @@
 
-
-
-function orderTotal() {
-  
+function orderTotal() 
+{
   let total = addTotal(getSelected());
   document.getElementById("displayTotal").textContent = "Your total for this order is:  $" + total.toFixed(2);
-  
 }
 
-function getSelected() {
-  
+function getSelected() 
+{
         //create array to hold selections
         let selected = new Array();
  
@@ -20,8 +17,10 @@ function getSelected() {
         let checks = beverages.getElementsByTagName("INPUT");
  
         // loops to push checked selections to an array
-        for (var i = 0; i < checks.length; i++) {
-            if (checks[i].checked) {
+        for (var i = 0; i < checks.length; i++) 
+        {
+            if (checks[i].checked) 
+            {
                 selected.push(checks[i].value);
             }
         }
@@ -30,12 +29,13 @@ function getSelected() {
   
 }
 
-function addTotal(selectedPrices) {
-
+function addTotal(selectedPrices) 
+{
   let sum = 0;
   
   // adds each selected beverage price to sum
-  for (var i = 0; i < selectedPrices.length; i++) {
+  for (var i = 0; i < selectedPrices.length; i++) 
+  {
     sum += parseFloat(selectedPrices[i]); 
   }
   
