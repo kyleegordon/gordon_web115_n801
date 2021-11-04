9d209df4ba4cac116ac01a83752e4f08663f4803
kyleegordon <87380075+kyleegordon@users.noreply.github.com>
2021-11-04 09:22:39 -0500

    Update intro_form.js

--- scripts/intro_form.js
+++ scripts/intro_form.js
@@ -2,43 +2,28 @@ function getData()
 {
   var firstName = document.getElementById('first_name').value;
   var lastName = document.getElementById('last_name').value;
-  
   var computerPlatform = document.getElementById('computer_platform').value;
   var personalBackground = document.getElementById('personal_background').value;
   var academicBackground = document.getElementById('academic_background').value;
+  var subjectBackground = document.getElementById('subject_background').value;
+  var courses = document.getElementById('courses').value;
+  var interestingFact = document.getElementById('interesting_fact').value; 
   
-  var firstWord = document.getElementById('first_word').value;
-  var secondWord = document.getElementById('second_word').value;
-  var thirdWord = document.getElementById('third_word').value;
-  
-  var totalLines = document.getElementById('total_lines').value;
-  
-  if (middleInitial && firstName && lastName) 
-  {
-     document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + " " + middleInitial + ". " + lastName + "!";
-  } 
-  else if (!middleInitial && firstName && lastName)
-   {
-     document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + " " + lastName + "!";
-   } 
-  else if (!middleInitial && firstName && !lastName)
-   {
-    document.getElementById("greeting").textContent = "Welcome to Kyko Kombucha, " + firstName + "!"; 
-   }
-  
-  
-  bubbleSip(firstDivisor, secondDivisor, thirdDivisor, firstWord, secondWord, thirdWord, totalLines);
+  displayData(firstName, lastName, computerPlatform, personalBackground, academicBackground, subjectBackground, courses, interestingFact);
 }
 
 
-function displayData(firstDivisor, secondDivisor, thirdDivisor, firstWord, secondWord, thirdWord, totalLines) 
+function displayData(firstName, lastName, computerPlatform, personalBackground, academicBackground, subjectBackground, courses, interestingFact) 
 {
+  let formInfo = [firstName, lastName, computerPlatform, personalBackground, academicBackground, subjectBackground, courses, interestingFact];
     
+  for (let i = 0; i < formInfo.length; i++)
+  {
     //creates new element & stores in variable
     var newListItem = document.createElement('li');
     
     //creates text node & stores in variable
-    var newText = document.createTextNode(word);
+    var newText = document.createTextNode(formInfo[i]);
     
     //attaches text node to new element
     newListItem.appendChild(newText);
